9ef51d38bdab66e27fca949ae85fdb606e4199a1
KencyOseguera <kencyoseguera7@gmail.com>
2024-04-14 10:24:56 -0600

    Asociacion de home con index

--- app.js
+++ app.js
@@ -32,6 +32,23 @@ const server = http.createServer((req, res) => {
     // Manejar la solicitud para la página principal index.html
     if (req.url === '/' && req.method === 'GET') {
         // Leer el archivo HTML de index.html
+        fs.readFile('home.html', 'utf8', (err, data) => {
+            if (err) {
+                console.error('Error al leer el archivo HTML:', err);
+                res.writeHead(500, { 'Content-Type': 'text/plain' });
+                res.end('Error interno del servidor');
+                return;
+            }
+
+            // Enviar el HTML como respuesta al cliente
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end(data);
+        });
+    }
+
+    // Manejar la solicitud para la página principal index.html
+    else if (req.url === '/index' && req.method === 'GET') {
+        // Leer el archivo HTML de index.html
         fs.readFile('index.html', 'utf8', (err, data) => {
             if (err) {
                 console.error('Error al leer el archivo HTML:', err);
